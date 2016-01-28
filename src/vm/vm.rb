086be6f6c2ff300344dcc94ee87559c1e30fb483
Alex Wang <ice_b0und@hotmail.com>
2016-01-28 19:58:34 +0800

    add vm integer

--- src/vm/vm.rb
+++ src/vm/vm.rb
@@ -1,7 +1,10 @@
+require 'json'
+
 module AlexRuby
   module VM
     module Errors
       TypeError = ::Class.new(RuntimeError)
+      ArgumentError = ::Class.new(ArgumentError)
     end
 
     module Type
@@ -9,25 +12,33 @@ module AlexRuby
         val.is_a?(Fixnum) ||
             val.is_a?(String) ||
             val.is_a?(Symbol) ||
-            val.is_a?(Float)
+            val.is_a?(Float) ||
+            val.is_a?(Array)
       end
     end
 
-    class Object
+    class Value
+      attr_accessor :type
+      attr_accessor :object
+      def initialize(type, object)
+        self.type = type
+        self.object = object
+      end
+    end
+
+    class Object < Value
       attr_accessor :clazz, :instance_variables
       def initialize(clazz)
+        super(:object, self)
         self.clazz = clazz
         self.instance_variables = {}
       end
-
-      def to_json
-
-      end
     end
 
-    class Class
+    class Class < Value
       attr_accessor :name, :super, :instance_methods, :vm, :outer_scope_constant
       def initialize(vm, name, outer_scope_constant = nil, superclass = nil, first_class = false)
+        super(:class, self)
         self.vm = vm
 
         if first_class
@@ -54,11 +65,15 @@ module AlexRuby
       def define_instance_method(name, scope)
         self.instance_methods[name] = Method.new(self, name, :normal, scope)
       end
+      def define_instance_method_block(name, &block)
+        self.instance_methods[name] = Method.new(self, name, :block, block)
+      end
       def define_builtin_instance_method(name, &block)
         self.instance_methods[name] = Method.new(self, name, :builtin, block)
       end
 
     end
+
 
     class Top < Class
       def initialize(vm)
@@ -99,7 +114,7 @@ module AlexRuby
       attr_accessor :outer_scope
 
       # an array of instructions
-      # instruction => [name, p1, p2...]
+      # instruction => [{name: name, params: xx}, ...]
       attr_accessor :instructions
       attr_accessor :vm
 
@@ -189,22 +204,69 @@ module AlexRuby
     end
 
     module Instructions
+      def self.included(othermod)
+        def othermod.ins(name, in_params = {}, out_params = {}, immediate = {}, &block)
+          define_method("ins_#{name}".to_sym) do |imp = {}|
 
-      # @param:   immediate value
-      # @used:
-      # @produced: immediate value
-      def ins_push_immediate(val)
-        if Type.check_immediate(val)
-          stack << val
-        else
-          raise Errors::TypeError
+            real_in_params = {}
+            in_params_array = stack.pop(in_params.size)
+            in_params.each_with_index do |pair, index|
+              k, v = pair
+              if v.nil?
+                real_in_params[k] = in_params_array[index]
+              elsif v.is_a?(::Proc)
+                if v.call(in_params_array[index])
+                  real_in_params[k] = in_params_array[index]
+                else
+                  raise Errors::TypeError
+                end
+              elsif in_params_array[index].is_a?(v)
+                real_in_params[k] = in_params_array[index]
+              else
+                raise Errors::TypeError.new("name: #{name}, in:#{in_params}, out:#{out_params}")
+              end
+            end
+
+            real_im_params = {}
+            immediate.each do |k, v|
+              if imp.key?(k)
+                if v.nil?
+                  real_im_params[k] = imp[k]
+                elsif v.is_a?(::Proc)
+                  if v.call(imp[k])
+                    real_im_params[k] = imp[k]
+                  else
+                    raise Errors::TypeError
+                  end
+                elsif imp[k].is_a?(v)
+                  real_im_params[k] = imp[k]
+                else
+                  raise Errors::TypeError
+                end
+              else
+                raise Errors::ArgumentError
+              end
+            end
+
+            outp = yield self, real_in_params, real_im_params
+
+            out_params.each do |k, v|
+              if v.nil?
+                stack.push outp[k]
+              elsif v.is_a?(::Proc)
+                if v.call(outp[k])
+                  stack.push outp[k]
+                else
+                  raise Errors::TypeError
+                end
+              elsif outp[k].is_a?(v)
+                stack.push outp[k]
+              else
+                raise Errors::TypeError
+              end
+            end
+          end
         end
-      end
-      def ins_pop
-        stack.pop
-      end
-      def ins_dup
-        stack.push(stack.last)
       end
 
       def ins_save_to_local
@@ -216,34 +278,8 @@ module AlexRuby
         local_name = stack.pop
         stack.push(current_scope.local_table[local_name])
       end
-
-      def ins_call_method
-        params = stack.pop
-        receiver = stack.pop
-        method_name = stack.pop.to_sym
-
-        call_method(receiver, method_name, params)
-        stack.push(nil)
-      end
-      def ins_new_object
-        clazz = stack.pop
-        stack.push(clazz.create_object)
-      end
-
-      def ins_load_constant
-        constant_name = stack.pop
-        stack.push(constant_table[constant_name])
-      end
-
-      def ins_define_class
-
-      end
-      def ins_define_instance_method
-
-      end
-
-
     end
+
     class Machine
       attr_accessor :stack
 
@@ -264,42 +300,160 @@ module AlexRuby
       end
 
       include Instructions
+
+      ins(:push_immediate,
+          { },
+          { :immediate => nil },
+          { :immediate => lambda { |v| Type.check_immediate(v) } }
+      ) do |vm, p, im|
+        { immediate: im[:immediate] }
+      end
+
+      ins(:pop, { item: nil }) do |vm, p, im|
+        {}
+      end
+
+      ins(:load_constant,
+          { constant_name: ::String },
+          { constant: Value }
+      ) do |vm, p, im|
+        { constant: vm.constant_table[p[:constant_name]] }
+      end
+
+      ins(:dup,
+          { value: nil },
+          { value: nil, value1: nil }
+      ) do |vm, p, im|
+        { value: p[:value], value1: p[:value] }
+      end
+
+      ins(:call_instance_method,
+          { name: ::String, receiver: Value, params: ::Array },
+          { ret: nil }
+      ) do |vm, p, im|
+        { ret: vm.call_instance_method(p[:name], p[:receiver], p[:params])}
+      end
+
+      ins(:define_class,
+          { outer_scope: Scope, superclass_full_name: String, name: String },
+          { clazz: Class }
+      ) do |vm, p, im|
+        { clazz: define_class(p[:name], p[:superclass_full_name], p[:outer_scope]) }
+      end
+
+      ins(:new_object,
+          { clazz: Class },
+          { object: Object }
+      ) do |vm, p, im|
+        object = p[:clazz].create_object
+        # call_instance_method('respond_to?'.to_sym, object, [:initialize])
+        { object: object }
+      end
+
+      ins(:iv_set,
+          { object: nil, name: String, value: nil }
+      ) do |vm, p, im|
+        p[:object].instance_variables[p[:name]] = p[:value]
+        {}
+      end
+
+      ins(:iv_get,
+          { object: nil, name: String },
+          { value: nil }
+      ) do |vm, p, im|
+        { value: p[:object].instance_variables[p[:name]] }
+      end
+
+      ins(:local_set_l0,
+          { name: String, value: nil }
+      ) do |vm, p, im|
+        vm.current_scope.local_table[p[:name]] = p[:value]
+        {}
+      end
+      ins(:local_get_l0,
+          { name: String },
+          { value: nil }
+      ) do |vm, p, im|
+        value = vm.current_scope.local_table[p[:name]]
+        { value: value }
+      end
+
+      ins(:nop) { |vm, p, im| {} }
+      ins(:params,
+          { },
+          { params: nil },
+          { count: Integer }
+      ) do |vm, p, im|
+        { params: vm.stack.pop(im[:count]) }
+      end
+
+      def eval_instruction(instruction)
+        name = instruction.first
+        params = instruction.size == 2 ? instruction.last : []
+        begin
+          send("ins_#{name}".to_sym, params)
+        rescue Exception => e
+          puts instruction.inspect
+          raise e
+        end
+      end
+
       def execute(start = 0)
-
-        # if @scopes.size > start
-        #   @current_scope = @scopes[start]
-        #
-        #   loop do
-        #   end
-        #
-        # end
-
         # initialize the infrastructures
         self.top_scope = Scope.new(self, :top)
+        self.constant_table[''] = self.top_scope
         self.current_scope = self.top_scope
         define_class  'Class'
+        define_class  'Integer'
+
         define_builtin_instance_method 'puts', 'Object' do |obj, str|
           puts str
         end
-        ins_push_immediate  'Object'
+        define_builtin_instance_method 'gets', 'Object' do |obj|
+          gets
+        end
+        define_instance_method_block 'initialize', 'Integer' do |obj, value|
+          obj.object.instance_variables[:value] = value
+        end
+        define_instance_method_block '+', 'Integer' do |obj, other|
+          obj.object.instance_variables[:value] + other.object.instance_variables[:value]
+        end
+
+        ins_push_immediate immediate: 'Object'
         ins_load_constant
         ins_new_object
         ins_dup
+
         obj = @stack.last
-        call_method('puts', obj, 'hello world!')
+        call_instance_method('puts', obj, 'hello world!')
+
+        open('../../test/vm.json') do |f|
+          current_scope.instructions = JSON.parse(f.read, symbolize_names: true)
+        end
+
+        current_scope.instructions.each do |ins|
+          puts "ins:#{ins}, stack:#{stack.last(3)}"
+          eval_instruction(ins)
+
+        end
 
       end
 
-      def define_class(name, superclass_full_name = nil)
-        outer_scope_constant = current_scope ? constant_table[current_scope.name] : top_scope
+      def define_class(name, superclass_full_name = nil, outer_scope = nil)
+        outer_scope_constant = outer_scope || constant_table[top_scope.constant_full_name]
         superclass = superclass_full_name ? constant_table[superclass_full_name] : constant_table['Object']
         clazz = Class.new(self, name, outer_scope_constant, superclass)
         constant_table[clazz.name] = clazz
       end
 
-      def define_instance_method(name, class_name, scope)
+      def define_instance_method(name, class_name, method_scope)
         clazz = constant_table[class_name]
-        clazz.define_instance_method(name, scope)
+        clazz.define_instance_method(name, method_scope)
+      end
+
+      def define_instance_method_block(name, class_name, &block)
+        clazz = constant_table[class_name]
+        clazz.define_instance_method_block(name, &block)
       end
 
       def define_builtin_instance_method(name, class_name, &block)
@@ -307,15 +461,19 @@ module AlexRuby
         clazz.define_builtin_instance_method(name, &block)
       end
 
-      def call_method(method_name, obj, params)
-        method = obj.clazz.instance_methods[method_name]
+      def call_instance_method(method_name, receiver, params)
+        method = receiver.clazz.instance_methods[method_name]
         if method.type == :builtin
-          ret = obj.send(method_name.to_sym, *params)
+          ret = receiver.send(method_name.to_sym, *params)
+          puts "=> #{ret.inspect}"
+          ret
+        elsif method.type == :block
+          ret = method.scope.call(receiver, *params)
           puts "=> #{ret.inspect}"
           ret
         else
           # prepare to call the method
-          puts "call #{method_name} on #{obj}"
+          puts "call #{method_name} on #{receiver}"
         end
       end
 
